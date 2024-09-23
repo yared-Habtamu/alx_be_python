c0c23b4e82d465bdfe01e64905f8d9824d95c063
Yared <yaredhabtamu08@gmail.com>
2024-09-23 21:42:20 +0300

    Project 1

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,11 @@
+def perform_operation(num1,num2,operation):
+    if operation == 'add':
+        return num1+num2
+    elif operation == 'substract':
+        return num1-num2
+    elif operation == 'multiply':
+        return num1*num2
+    else:
+        if num2 != 0:
+            return num1/num2
+        return "You can't divide a number by 0"
