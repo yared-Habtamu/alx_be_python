112294986164c1515432270191b403fdca751ad0
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-10-03 22:26:07 +0300

    Updated

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,3 +10,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1,1),0)
         self.assertEqual(self.calc.add(0,0),0)
         self.assertEqual(self.calc.add(-1,-1),-2)
+
+    def test_substraction(self):
+        self.assertNotEqual(self.calc.subtract(1,-1),1)
+        self.assertEqual(self.calc.subtract(10,10),0)
