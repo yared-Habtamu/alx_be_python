32be8d66cc2aafaa6c987773c18f969181c76897
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-10-03 16:35:10 +0300

    Updated

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,4 +12,4 @@ class BankAccount:
         return True
 
     def display_balance(self):
-        return f"Current Balance: ${self.account_balance}"
+        return f"Current Balance: ${self.account_balance:.2f}"
