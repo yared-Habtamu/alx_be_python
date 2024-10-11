7161477efcec679fceea2e30816b4375a4caf093
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-10-12 00:40:04 +0300

    Corrected

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,7 +12,7 @@ class EBook(Book):
         self.file_size=file_size
 
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
   
 
 class PrintBook(Book):
@@ -23,7 +23,7 @@ class PrintBook(Book):
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
-class Library():
+class Library:
     def __init__(self):
         self.books=[]
         
