374e79cbd6c11424c676f02c5811d135fa2bff1f
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-10-12 13:30:44 +0300

    Well done

---
+++ oop/polymorohism_demo.py
@@ -0,0 +1,20 @@
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError("Not Implementd")
+
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        # super().__init__(area)
+        self.length=length
+        self.width=width
+
+    def area(self):
+        return self.length*self.width
+        
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius=radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
